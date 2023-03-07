a7c6f0f2b82c455229d018f2106e54b0d324c592
pyncer <root@pyncer.com>
2023-03-07 02:00:17 -0500

    Added snyppets parameter to SnyppetManager.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,12 @@
 # Change Log
 
+## 1.0.1 - 2023-03-07
+
+### Changed
+
+- Added snyppets parameter to SnyppetManager constructor to optionally limit
+  what snyppets should be used.
+
 ## 1.0.0 - 2023-03-07
 
 Initial release.
--- src/SnyppetManager.php
+++ src/SnyppetManager.php
@@ -2,18 +2,49 @@
 namespace Pyncer\Snyppet;
 
 use Pyncer\Snyppet\Snyppet;
+use Pyncer\Snyppet\Exception\SnyppetNotFoundException;
 use Pyncer\Iterable\StaticAccessIterator;
 
 class SnyppetManager extends StaticAccessIterator
 {
-    public function __construct()
-    {
+    /**
+     * @param null|array<string> $snyppets An array of snyppet aliases to
+     *  limit what snyppets are available.
+     */
+    public function __construct(
+        protected ?array $snyppets = null
+    ) {
         $this->initialize();
+    }
+
+    public function rewind(): void
+    {
+        if ($this->snyppets === null) {
+            static::$keys = array_keys(static::$values);
+        } else {
+            // Limit iterator to enabled snyppets
+            $keys = array_keys(static::$values);
+
+            static::$keys = [];
+
+            foreach ($this->snyppets as $snyppet) {
+                if (in_array($snyppet, $keys)) {
+                    static::$keys[] = $snyppet;
+                }
+            }
+        }
+        static::$position = 0;
     }
 
     public function initialize(): void
     {
         foreach (static::$values as $snyppet) {
+            if ($this->snyppets !== null &&
+                !in_array($snyppet->getAlias(), $this->snyppets)
+            ) {
+                continue;
+            }
+
             $snyppet->initialize();
         }
     }
@@ -25,11 +56,27 @@ class SnyppetManager extends StaticAccessIterator
 
     public function get(string $alias): SnyppetInterface
     {
+        if ($this->snyppets !== null &&
+            !in_array($alias, $this->snyppets)
+        ) {
+            throw new SnyppetNotFoundException($alias);
+        }
+
+        if (!array_key_exists($alias, static::$values)) {
+            throw new SnyppetNotFoundException($alias);
+        }
+
         return static::$values[$alias];
     }
 
     public function has(string $alias): bool
     {
+        if ($this->snyppets !== null &&
+            !in_array($alias, $this->snyppets)
+        ) {
+            return false;
+        }
+
         return array_key_exists($alias, static::$values);
     }
 }
